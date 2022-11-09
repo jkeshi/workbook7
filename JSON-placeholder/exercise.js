a9fdd3b6c76f9f440580e71cde438123f1aa7355
jkesh <jkeshi@gmail.com>
2022-11-09 13:19:59 -0500

    exercise warmup

---
+++ JSON-placeholder/exercise.js
@@ -0,0 +1,39 @@
+//function decleration
+
+function Hello (name) {
+    
+}
+//function expression
+Const hello = () => {
+
+}
+const hello = function (name)  {
+    console.log(name);
+}
+
+//arrow function
+(first) => {
+    second;
+};
+
+
+//object
+let animal = {
+    name: 'hermano',
+
+    hasfur: false
+
+}
+
+//array
+let mumber = [1,2,3,4];
+
+let course = [
+    { courseName: "intro to JS"}
+];
+
+for (const course of courses) {
+}
+
+let messageDiv = "";
+const messageDiv = "";
