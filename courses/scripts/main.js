9ad4b8aa30be0ef9f4deb7ec4f2c6150bc926a6c
jkesh <jkeshi@gmail.com>
2022-11-09 15:55:13 -0500

    update exercise

---
+++ courses/scripts/main.js
@@ -0,0 +1,26 @@
+const courseTableBody = document.querySelector("#courseTable tbody");
+
+function loadCourseTable() {
+  fetch("http://localhost:8081/api/courses")
+    .then((response) => response.json())
+    .then((courses) => {
+      console.log(courses);
+      courses.forEach((course) => {
+        let row = courseTableBody.insertRow(-1);
+        let cell1 = row.insertCell(0);
+        let cell2 = row.insertCell(1);
+        let cell3 = row.insertCell(2);
+        cell1.innerText = course.id;
+        cell2.innerText = course.dept;
+        const anchor = document.createElement("a");
+        anchor.innerText = course.courseName;
+        anchor.href = "./details.html?courseid=${course.id}";
+
+        cell3.appendChild(anchor);
+      });
+    });
+}
+
+window.onload = () => {
+  loadCourseTable();
+};
