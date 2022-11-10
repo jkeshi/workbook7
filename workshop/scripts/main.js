08ab9c56ff4427649921abe8afcd4c39a485cacf
jkesh <jkeshi@gmail.com>
2022-11-10 11:30:21 -0500

    update

--- workshop/scripts/main.js
+++ workshop/scripts/main.js
@@ -3,17 +3,26 @@
 const selectDropdown = document.getElementById("select-dropdown");
 const categoryDropdown = document.getElementById("category-dropdown");
 function loadCategoryDropdown() {
- categoryDropdown.style.display = none;
-    selectValue = selectDropdown.value
+  //categoryDropdown.style.display = none;
+  selectValue = selectDropdown.value;
+  selectValue = categoryDropdown.value;
 
   fetch("http://localhost:8081/api/categories")
-    .then((response) => response.json)()
+    .then((response) => response.json())
     .then((categories) => {
-        if (selectValue === all) {
-            
-        }
+      console.log(categories);
+      categories.forEach((category) => {
+        let row = courseTableBody.insertRow(-1);
+        let cell1 = row.insertCell(0);
+        let cell2 = row.insertCell(1);
+        let cell3 = row.insertCell(2);
+        cell1.innerText = category.categoryId;
+        cell2.innerText = category.Description;
+        cell3.innerText = category.categoriesName;
+      });
     });
 }
 window.onload = () => {
   selectDropdown.onchange = loadCategoryDropdown;
+  loadCategoryDropdown();
 };
